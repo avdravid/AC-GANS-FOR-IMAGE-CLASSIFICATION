99a0e28d40d30eaf41766e89a62c63eb9adf71c3
Amil Dravid <46203730+avdravid@users.noreply.github.com>
2021-10-27 15:25:43 -0500

    Update CNN_Model_Baseline_Fashion-MNIST.py

--- CNN_Model_Baseline_Fashion-MNIST.py
+++ CNN_Model_Baseline_Fashion-MNIST.py
@@ -6,7 +6,6 @@ class Baseline_CNN(torch.nn.Module):
         super(Discriminator, self).__init__()
 
         self.main = torch.nn.Sequential(
-        
             torch.nn.Conv2d(
                 in_channels= 1,
                 out_channels=ndf * 2,
@@ -18,7 +17,6 @@ class Baseline_CNN(torch.nn.Module):
             torch.nn.BatchNorm2d(num_features= 64 * 2),
             torch.nn.LeakyReLU(negative_slope=0.2, inplace=True),
 
-            
             torch.nn.Conv2d(
                 in_channels=ndf * 2,
                 out_channels=ndf * 4,
@@ -46,7 +44,7 @@ class Baseline_CNN(torch.nn.Module):
         # Categorical Classifier
         self.clf = torch.nn.Sequential(
             torch.nn.Linear(
-                in_features=64 * 8 * 3 * 3,
+                in_features= 64 * 8 * 3 * 3,
                 out_features= 10,
                 bias=True
             ),
